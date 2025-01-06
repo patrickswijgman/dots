4c998a8ed680578f3881e0fa8d1be9ae41df3680
Patrick <petrik09@live.nl>
2025-01-06 19:21:39 +0100

    chore: config update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,14 +7,16 @@ abbr gst 'git stash'
 abbr gstp 'git stash pop'
 abbr gm 'git commit -m'
 abbr gma 'git commit --amend'
+abbr gf 'git fetch'
 abbr gp 'git pull'
 abbr gP 'git push'
 abbr gl 'git log'
 abbr gd 'git diff'
-abbr gr 'git rebase -i main'
+abbr gr 'git rebase -i'
+abbr grc 'git rebase --continue'
+abbr gra 'git rebase --abort'
 abbr grs 'git restore'
 abbr grss 'git restore --staged'
 
 fish_add_path ~/.local/bin
 fish_add_path ~/go/bin
-fish_add_path /usr/local/go/bin
